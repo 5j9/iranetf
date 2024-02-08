8dbbf1d2a7bc0c82973618edfc9e95b2052ca9e4
5j9 <5j9@users.noreply.github.com>
2024-02-08 14:41:14 +0330

    feat(Rahavard365): add values method

--- iranetf/rahavard365.py
+++ iranetf/rahavard365.py
@@ -1,9 +1,12 @@
+from json import loads
+
 from aiohutils.session import SessionManager
 from pandas import DataFrame
 
 session_manager = SessionManager()
 
-API = 'https://rahavard365.com/api/v2/'
+HOME = 'https://rahavard365.com/'
+API = f'{HOME}api/v2/'
 
 
 async def api(path: str) -> dict:
@@ -19,6 +22,16 @@ class Rahavard365:
 
     async def specification(self) -> dict:
         return await api(f'asset/{self.asset_id}/specification')
+
+    async def values(self) -> tuple[dict, DataFrame]:
+        r = await session_manager.get(f'{HOME}asset/{self.asset_id}/values')
+        text = await r.text()
+        start = text.find('var layoutModel = ') + 18
+        end = text.find(';', start)
+        j = loads(text[start:end])
+        if (funds := j.pop('funds', None)) is not None:
+            j['funds'] = DataFrame(funds)
+        return j
 
 
 async def etfs(short_name=False) -> DataFrame:
