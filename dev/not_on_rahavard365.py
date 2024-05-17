30dc4d4365fc96c4248f53db66c29f13aed53259
5j9 <5j9@users.noreply.github.com>
2024-05-17 18:32:27 +0330

    fix(not_on_rahavard365): update column name

--- dev/not_on_rahavard365.py
+++ dev/not_on_rahavard365.py
@@ -8,5 +8,5 @@ ds = load_dataset(site=False)
 
 rdf = run(etfs(short_name=True))
 
-missing_in_ds = rdf['short_name'][~rdf['short_name'].isin(ds['symbol'])]
+missing_in_ds = rdf['short_name'][~rdf['short_name'].isin(ds['l18'])]
 print(missing_in_ds)
