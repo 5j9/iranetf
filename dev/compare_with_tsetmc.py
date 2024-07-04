cc871015cf5b07cbcee5ef1a35ee7c84cb102cdf
5j9 <5j9@users.noreply.github.com>
2024-07-04 11:00:14 +0330

    dev(compare_with_tsetmc.py): ignore PE ETFs

--- dev/compare_with_tsetmc.py
+++ dev/compare_with_tsetmc.py
@@ -2,8 +2,21 @@ from asyncio import run
 
 from pandas import concat
 from tsetmc.funds import commodity_etfs, etfs
+from tsetmc.instruments import Instrument
 
 from iranetf import load_dataset
+
+ignore_desc = {'نوع صندوق : خصوصی'}
+
+
+async def is_valid(l18: str) -> bool:
+    inst = await Instrument.from_l18(l18)
+    info = await inst.info()
+    desc = info['faraDesc']
+    if desc in ignore_desc:
+        return False
+    print(f'{l18=!r} {desc=!r}')
+    return True
 
 
 async def main():
@@ -18,7 +31,10 @@ async def main():
     df_set = set(df['instrument.lVal18AFC'])
 
     print(f'{ds_set - df_set = }')
-    print(f'{df_set - ds_set = }')
+
+    not_in_ds = [i for i in df_set - ds_set if await is_valid(i)]
+
+    print(f'{not_in_ds = }')
 
 
 run(main())
