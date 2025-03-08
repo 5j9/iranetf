1b557b5785c07ff560a7c76d1981381239150263
5j9 <5j9@users.noreply.github.com>
2025-03-08 17:38:12 +0330

    chore(__version__): bump to 0.22.4.dev0

--- iranetf/__init__.py
+++ iranetf/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.22.3'
+__version__ = '0.22.4.dev0'
 import logging as _logging
 from abc import ABC as _ABC, abstractmethod as _abstractmethod
 from asyncio import gather as _gather
