dc78e38e431df228aa964d4a6f41d8a32d9d4950
5j9 <5j9@users.noreply.github.com>
2025-05-15 13:20:44 +0330

    tests: rename ltp to harom

--- tests/test_tadbirpardaz_leveraged.py
+++ tests/test_tadbirpardaz_leveraged.py
@@ -11,20 +11,19 @@ from tests import assert_navps_history
 from tests.test_tadbirpardaz import EXPECTED_TP_VER
 
 tavan: LeveragedTadbirPardaz = BaseSite.from_l18('توان')  # type: ignore
-
-ltp: LeveragedTadbirPardaz = BaseSite.from_l18('اهرم')  # type: ignore
+ahrom: LeveragedTadbirPardaz = BaseSite.from_l18('اهرم')  # type: ignore
 
 
 @file('ahrom_live.json')
 async def test_live_navps_leveraged():
-    live = await ltp.live_navps()
+    live = await ahrom.live_navps()
     assert_dict_type(live, LeveragedTadbirPardazLiveNAVPS)
 
 
 @file('ahrom_navps_history.json')
 async def test_navps_history_leveraged():
     # leveraged ETFs do not have statistical history for preferred shares
-    await assert_navps_history(ltp, has_statistical=False)
+    await assert_navps_history(ahrom, has_statistical=False)
 
 
 @file('tavan_float.json')
@@ -35,17 +34,17 @@ async def test_float_base_units_value():
 
 @file('leveraged_tadbir_version.html')
 async def test_leveraged_version():
-    assert (await ltp.version()) == EXPECTED_TP_VER
+    assert (await ahrom.version()) == EXPECTED_TP_VER
 
 
 @file('ahrom_aa.json')
 async def test_asset_allocation():
-    aa = await ltp.asset_allocation()
-    assert aa.keys() <= ltp._aa_keys
+    aa = await ahrom.asset_allocation()
+    assert aa.keys() <= ahrom._aa_keys
     assert isclose(sum(aa.values()), 1.0)
 
 
 @file('ahrom_aa.json')
 async def test_cache():
-    cache = await ltp.cache()
+    cache = await ahrom.cache()
     assert 0.0 <= cache <= 0.6
