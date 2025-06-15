cb531a6d8c760f7db38169d830249cb716f1bd51
5j9 <5j9@users.noreply.github.com>
2025-06-15 22:24:28 +0330

    release: v0.25.2

--- iranetf/__init__.py
+++ iranetf/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.25.2.dev0'
+__version__ = '0.25.2'
 import logging as _logging
 from abc import ABC as _ABC, abstractmethod as _abstractmethod
 from asyncio import gather as _gather
