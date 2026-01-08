e85ed330341332c4cd12c0d1bac175f8ff2f8f9d
5j9 <5j9@users.noreply.github.com>
2026-01-08 14:40:06 +0330

    feat: use rich logs for dev

--- dev/check_dataset.py
+++ dev/check_dataset.py
@@ -1,6 +1,22 @@
+import logging
 from asyncio import run
 
+from rich.logging import RichHandler
+
 from iranetf.dataset import check_dataset
+
+logging.basicConfig(
+    level='NOTSET',
+    format='%(message)s',
+    datefmt='[%X]',
+    handlers=[
+        RichHandler(
+            show_path=True,  # Keep the path on the right
+            enable_link_path=True,  # Specifically enables terminal hyperlinks
+            rich_tracebacks=True,
+        )
+    ],
+)
 
 
 async def main():
