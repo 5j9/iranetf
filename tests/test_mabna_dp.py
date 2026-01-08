adf66768a50f0be5aa2f35869da95ec691a70560
5j9 <5j9@users.noreply.github.com>
2026-01-08 14:49:52 +0330

    from_l18 instead of from_url
    hamvazn is no longer v1, it is now MabnaDB2

--- tests/test_mabna_dp.py
+++ tests/test_mabna_dp.py
@@ -6,12 +6,12 @@ from pytest_aiohutils import file, validate_dict
 from iranetf.sites import LiveNAVPS, MabnaDP
 from tests import assert_navps_history
 
-hamvazn = MabnaDP('https://kianfunds6.ir/')
+site = MabnaDP.from_l18('رویش همراه')
 
 
 @file('hamvasn_live.json')
 async def test_live_navps_mabna():
-    d = await hamvazn.live_navps()
+    d = await site.live_navps()
     assert type(d.pop('date_time')) is str
     assert type(d.pop('statistical_price')) is float
     assert type(d.pop('unit_count')) is int
@@ -20,13 +20,13 @@ async def test_live_navps_mabna():
 
 @file('hamvazn_navps_history.json')
 async def test_navps_history_mabna():
-    await assert_navps_history(hamvazn)
+    await assert_navps_history(site)
 
 
 @file('mabna_version.html')
 async def test_mabna_version():
-    assert (await hamvazn.version()) == '2.15'
-    assert await hamvazn.reg_no() == '11924'
+    assert (await site.version()) == '2.22'
+    assert await site.reg_no() == '12436'
 
 
 @mark.xfail
@@ -37,12 +37,12 @@ async def test_old_mabna_version():
 
 @file('mabna_aa.json')
 async def test_asset_allocation():
-    aa = await hamvazn.asset_allocation()
-    assert aa.keys() <= hamvazn._aa_keys
+    aa = await site.asset_allocation()
+    assert aa.keys() <= site._aa_keys
     assert isclose(sum(aa.values()), 1.0, abs_tol=0.0000000001)
 
 
 @file('mabna_aa.json')
 async def test_cache():
-    cache = await hamvazn.cache()
+    cache = await site.cache()
     assert 0.0 <= cache <= 0.6
