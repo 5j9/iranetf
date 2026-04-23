06a46000a6101201835282d2e8fd6618ec04d270
5j9 <5j9@users.noreply.github.com>
2026-04-23 18:32:46 +0330

    better naming, use logger

--- dev/compare_with_rahavard365.py
+++ dev/compare_with_rahavard365.py
@@ -1,5 +1,6 @@
 from asyncio import run
 
+from dev import logger
 from iranetf.dataset import load_dataset
 from iranetf.rahavard365 import etfs
 
@@ -9,4 +10,4 @@ ds = load_dataset(site=False)
 rdf = run(etfs(short_name=True))
 
 missing_in_ds = rdf['short_name'][~rdf['short_name'].isin(ds['l18'])]
-print(missing_in_ds)
+logger.info(missing_in_ds)
