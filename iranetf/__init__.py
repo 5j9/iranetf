fea2524ef8cbd866415f6e21c6f6c67156703df1
5j9 <5j9@users.noreply.github.com>
2026-04-30 17:48:40 +0330

    feat: use DEFAULT_TIMEOUT

--- iranetf/__init__.py
+++ iranetf/__init__.py
@@ -3,9 +3,10 @@ __version__ = '4.0.2.dev1'
 from aiohttp import (
     ClientResponse as _ClientResponse,
 )
+from aiohttp.client import DEFAULT_TIMEOUT as _DEFAULT_TIMEOUT
 from aiohutils.session import SessionManager
 
-session_manager = SessionManager()
+session_manager = SessionManager(timeout=_DEFAULT_TIMEOUT)
 
 
 ssl: bool = False  # as horrible as this is, many sites fail ssl verification
